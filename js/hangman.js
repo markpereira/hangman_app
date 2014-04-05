5a9141d1ec23a864c5b5ad52b4afba9ee58ffc1c
nixsiow <nixsiow@hotmail.com>
2014-04-05 14:58:21 +1100

    nix push2

--- js/hangman.js
+++ js/hangman.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
 
+	// All the variables live here : -----------------------------------
     var words = ["Skynet", "Digital", "FitBit", "Realtime", "ManPacks", "Landing", "Conversion", "Social", "Airbnb", "SnapChat", "Bangedup", "Application", "Analytics", "Geolocation", "Nodeability", "KickStarter", "Matching", "Adults", "Pinterest", "Groupon", "Appstore"];
 
     var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 
     var word = _.sample(words).toUpperCase();
     console.log(word);
@@ -10,30 +12,41 @@ $(document).ready(function () {
     var split_word = word.split('');
 
     var $guessWords = $('#words');
+
+    // Empty arrays to store letters.
     var guessedLetters = [];
     var solution = [];
 
+	// ----------------- end of the variables -------------------
+
+
+
+    // Define hangmanSetup function -----------------------
     var hangmanSetup = function () {
 
+    	indexID = 0
 
 		_.each(split_word, function (letter, index) {
 	        $('#letters').append("<div class='solution_letter' id='" + index + "'>" + letter + "</div>");
-
+	        indexID = index
 	    });
 
+
+		// function to hide the secret word at the beginning
 	    var hideSolution = function () {
 			$('.solution_letter').hide();
 		};
 
-		var showSolution = function () {
-			$('.solution_letter').show();
+
+		// function to show the secret word's letter if the user pick the right letter.
+		window.showSolution = function () {  // Global variable, Im sorry
+			$('#' + indexID).show();
+			console.log(indexID)
 		};
 
-		hideSolution();
 
 
-
-
+		// Getting all the alphabets from array and print it on screen.
         _.each(letters, function (letter) {
             $('#container').append("<div class='letter'>" + letter + "</div>");
         });
@@ -43,7 +56,7 @@ $(document).ready(function () {
 
 
 
-
+        // when the user click on particular letter, fadeOut the letter.
         $letter.on('click', function () {
             $(this).fadeOut();
             var pickedLetter = $(this).html();
@@ -52,24 +65,26 @@ $(document).ready(function () {
             //call another function and pass in the letter;
         });
 
-    };
+
+    }; // end of the hangmanSetup function. -----------------------
 
 
+
+    // Push all the picked letter to an array named 'guessedLetter' and show all on the screen later.
     var letterValidation = function (letter) {
         guessedLetters.push(letter);
         console.log('guessletter ' + guessedLetters);
 
-        if (_.contains(word, letter)) {
+        // if (_.contains(word, letter)) {
 
-        } else {
+        // } else {
 
-        }
+        // }
 
     };
 
 
-    // Joel
-
+    // function to check whether the picked letters are inside the secret word, if yes then push to 'solution' array.
     var processGuess = function (l) {
         if (_(split_word).include(l)) {
             solution.push(l);
@@ -78,17 +93,16 @@ $(document).ready(function () {
         }
     };
 
-    // var $words = $('#word').empty();
-    // _(letters).each(function (wordletter) {
-    //     if (_(solution).include(wordletter)) {
-    //         $words.append('<span>' + wordletter + '</span>');
-    //     } else {
-    //         $words.append('<span>_</span>');
-    //     }
-    // });
 
 
-
-
+    // Call the hangmanSetup function whenever the page start/reload.
     hangmanSetup();
-});
+
+    // Call the hideSolution function whenever the page start/reload.
+    hideSolution();
+
+
+}); // end of the document.ready function. 
+
+
+
